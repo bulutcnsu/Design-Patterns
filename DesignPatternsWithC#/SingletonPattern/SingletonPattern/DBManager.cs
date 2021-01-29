06d030d23865757a5be880efc8325d7dc0201613
bulutcnsu <60491413+bulutcnsu@users.noreply.github.com>
2021-01-30 02:22:30 +0300

    Add files via upload

---
+++ DesignPatternsWithC#/SingletonPattern/SingletonPattern/DBManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPattern
+{
+    public class DBManager
+    {
+        private DBManager(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+        private static DBManager _manager;
+      
+        public static DBManager CreateDbManager(string connectionString)
+        {
+            if (_manager == null)
+            {
+                _manager = new DBManager(connectionString);
+            }
+
+           
+                return _manager;
+            
+        }
+
+        public string ConnectionString { get; set; }
+        public string StateMessage { get; set; }
+
+    }
+}
---
+++ DesignPatternsWithC#/SingletonPattern/SingletonPattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DBManager manager1 =  DBManager.CreateDbManager("connection");
+            manager1.StateMessage = "Message1";
+
+            DBManager manager2 = DBManager.CreateDbManager("connection2");
+            Console.WriteLine(manager2.StateMessage);
+
+            Console.ReadKey();
+
+        }
+    }
+}
